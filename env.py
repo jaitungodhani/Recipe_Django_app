33044ce762c306c7e00d9a6172946c84f2eef570
User <user@Users-MacBook-Pro.local>
2022-07-24 18:17:15 +0530

    change in requirements.txt

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://ialiwwpklkcrco:0257c7540614fb70ca6dfba6d16dc8154b8a969c3c87c747662b7dac02e45558@ec2-54-77-40-202.eu-west-1.compute.amazonaws.com:5432/dd0qkrig9881se"
-os.environ["SECRET_KEY"] = "Jaitun1999!!"
-os.environ["CLOUDINARY_URL"] = "cloudinary://625865289567944:FXHzvgBmnFpQ4ZFLXgLZYIEqv8s@drfdjango"
