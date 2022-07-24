0243ebf01449eff3a9d535f478fe0d95a9dba1f4
User <user@Users-MacBook-Pro.local>
2022-07-24 17:34:20 +0530

    change in requirements.txt

--- foodstar/settings.py
+++ foodstar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['foodstar2022.herokuapp.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['recipedjango.herokuapp.com', 'localhost','127.0.0.1']
 
 
 # Application definition
